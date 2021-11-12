bb3951b34e30e2926ed752338840ee3933458a81
Glogo20XX <adanjuma@ccfng.org>
2021-11-12 09:44:58 +0100

    First Commit

---
+++ ccfn-git-lad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# **************************************************************************************** #
+# ***********************CTRL --Ubuntu_20.04 --Glogo Automation*************************** #
+# **************************************************************************************** #
+
+# simple hands on labs demonstrating the clonining of files from Git repository to local repository and vice-versa
+# we are progressing 
+# Get serious --auto install sync
+# Thu 11 Nov 2021 14:41:58 
+
+ sudo systemctl daemon-reload
+ 
+ 
+ 
+ 
+ 
